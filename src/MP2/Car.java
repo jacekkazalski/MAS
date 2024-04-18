0e5ad53962d8303fd1838f84011d419432e30473
jacekkazalski <jacek.kazalski@gmai.com>
2024-04-18 17:05:40 +0200

    car

--- src/MP2/Car.java
+++ src/MP2/Car.java
@@ -72,7 +72,7 @@ public class Car {
     public class ServiceInformation {
         private final boolean passed;
         private final Date date;
-        private final Car car;
+        private Car car;
 
         public ServiceInformation(boolean passed, Date date, Car car) {
             this.passed = passed;
